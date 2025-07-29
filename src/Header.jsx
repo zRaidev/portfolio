1d5b8a911002e9305c41b0db195f555ce1a89af3
macRaidev <isaac14e2007@gmail.com>
2025-07-28 21:02:30 -0500

    tailwindcss implementation

---
+++ src/Header.jsx
@@ -0,0 +1,17 @@
+function Header () {
+  return (
+    <header className='bg-gray-800'>
+      <nav className='w-1/4 justify-center mx-auto'>
+        <ul className='flex justify-around'>
+          <li>Home</li>
+          <li>About Me</li>
+          <li>Projects</li>
+          <li>Habilities</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
+
+export default Header
