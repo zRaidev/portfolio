66148724aa1a580e63c7bb19a8922627b9314ef1
macRaidev <isaac14e2007@gmail.com>
2025-07-28 22:36:13 -0500

    Header

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 function Footer () {
   return (
-    <footer className='bg-gray-800 text-white p-4 text-center'>
+    <footer className='p-4 text-center bg-[var(--surface)]'>
       <p>&copy; {new Date().getFullYear()} Isaac Vergara. All rights reserved.</p>
       <p>Designed & Developed by Isaac Vergara.</p>
       <div>
@@ -11,7 +11,7 @@ function Footer () {
               const theme = e.target.value
               document.documentElement.classList.toggle('dark-theme', theme === 'dark')
             }}
-            className='ml-2 px-3 py-1 rounded bg-gray-700 text-white'
+            className='ml-2 px-3 py-1 rounded bg-[var(--text)] text-[var(--bg)]'
           >
             <option value='' />
             <option value='light'>Light</option>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,18 @@
 function Header () {
   return (
-    <header className='bg-[var(--primary)] w-full h-20 flex items-center shadow-md'>
-      <nav className='w-1/4 justify-center mx-auto'>
-        <ul className='flex justify-around'>
-          <li className=''>Home</li>
-          <li>About Me</li>
-          <li>Projects</li>
-          <li>Habilities</li>
-          <li>Contact</li>
-        </ul>
-      </nav>
-    </header>
+    <div className='w-full h-16'>
+      <header className='fixed top-0 left-0 w-full h-16 flex items-center justify-between px-4 z-50'>
+        <nav className='w-2/5 h-10 justify-center mx-auto'>
+          <ul className='flex justify-around text-[var(--text)] align-middle bg-[var(--surface)] px-2 py-2 rounded-2xl select-none'>
+            <li className=''>Home</li>
+            <li>About Me</li>
+            <li>Projects</li>
+            <li>Habilities</li>
+            <li>Contact</li>
+          </ul>
+        </nav>
+      </header>
+    </div>
   )
 }
 
