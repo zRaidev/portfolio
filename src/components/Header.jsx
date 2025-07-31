c33f66562f66f08112ce48010302e5a12c5b3cd8
Raidev <isaac14e2007@gmail.com>
2025-07-31 01:44:48 -0500

    Header including id href

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,11 +4,11 @@ function Header () {
       <header className='fixed top-0 left-0 w-full h-16 flex items-center justify-between px-4 z-50'>
         <nav className='w-2/5 h-10 justify-center mx-auto'>
           <ul className='flex justify-around text-[var(--text)] align-middle bg-[var(--surface)] px-2 py-2 rounded-2xl select-none'>
-            <li className=''>Home</li>
-            <li>About Me</li>
-            <li>Projects</li>
-            <li>Habilities</li>
-            <li>Contact</li>
+            <li><a href='#home'>Home</a></li>
+            <li><a href='#about-me'>About Me</a></li>
+            <li><a href='#projects'>Projects</a></li>
+            <li><a href='#habilities'>Habilities</a></li>
+            <li><a href='#contact'>Contact</a></li>
           </ul>
         </nav>
       </header>
