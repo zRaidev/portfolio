902fca4ff5c9080c060b34902d2a3bb81b212291
Raidev <isaac14e2007@gmail.com>
2025-08-02 00:37:16 -0500

    Uploading Projects section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Header from './components/Header.jsx'
 import Footer from './components/Footer.jsx'
 import Hero from './components/Hero.jsx'
 import AboutMe from './components/AboutMe.jsx'
+import Projects from './components/Projects.jsx'
 
 function App () {
   return (
@@ -10,6 +11,7 @@ function App () {
       <main className='max-w-4/5 mx-auto'>
         <Hero />
         <AboutMe />
+        <Projects />
       </main>
       <Footer />
     </>
