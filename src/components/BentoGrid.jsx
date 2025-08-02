9b91a7976210f868b3194f5c1120318b69e6f9db
Raidev <isaac14e2007@gmail.com>
2025-08-01 22:02:32 -0500

    Creating BentoGrid.jsx

--- src/components/BentoGrid.jsx
+++ src/components/BentoGrid.jsx
@@ -1,6 +1,11 @@
-function BentoGrid ({ children }) {
+function BentoGrid ({ children, className }) {
   return (
-    <div className='w-150 h-80 p-10 rounded-xl shadow-lg'>
+    <div
+      className={`p-10 border-2 border-white/10 backdrop-blur-lg shadow-[0_2px_20px_rgba(145,94,255,0.25)] rounded-2xl ${className || ''}`}
+      style={{
+        background: 'linear-gradient(135deg, rgba(145,94,255,0.1), rgba(123,104,238,0.07), rgba(255,255,255,0.0025))'
+      }}
+    >
       {children}
     </div>
   )
