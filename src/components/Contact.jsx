c6adebf1990a345315d3c8bca57d44ae826d0a6e
Raidev <isaac14e2007@gmail.com>
2025-08-17 17:44:25 -0500

    Fixing Bento Email

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -9,7 +9,9 @@ function Contact () {
       </div>
       <div className='flex gap-8 justify-center'>
         <BentoGrid className='w-100 text-center'>
-          <h4 className='text-2xl font-semibold mb-2'>✉️ Email</h4>
+          <h4 className='text-2xl font-semibold mb-2'>
+            <svg className='w-6 inline pb-1' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='none' stroke='currentColor' stroke-width='2' stroke-linecap='round' stroke-linejoin='round' role='img' aria-label='Email icon'><rect x='3' y='5' width='18' height='14' rx='2' ry='2' /><path d='M3 7l9 6 9-6' /></svg> Email
+          </h4>
           <p className='pb-5 opacity-80'>isaac14e2007@hotmail.com</p>
           <a href='REDIRECCIONAR PARA ENVIAR EL CORREO'>
             <button className='px-8 py-2 bg-[#4e0eff] rounded-xl cursor-pointer'>Send an email</button>
