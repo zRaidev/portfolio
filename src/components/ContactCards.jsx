c15bcf5bac88b0927a6e6a772f8ce31a820a152a
Raidev <isaac14e2007@gmail.com>
2025-08-17 18:36:44 -0500

    Implementing Contact functions

--- src/components/ContactCards.jsx
+++ src/components/ContactCards.jsx
@@ -1,13 +1,13 @@
 import BentoGrid from './BentoGrid.jsx'
 
-function ContactCards ({ svg, title, description, buttonText, href }) {
+function ContactCards ({ svg, title, description, buttonText, href, target }) {
   return (
     <BentoGrid className='w-100 text-center'>
       <h4 className='text-2xl font-semibold mb-2'>
         {svg} {title}
       </h4>
       <p className='pb-5 opacity-80'>{description}</p>
-      <a href={href}>
+      <a href={href} target={target}>
         <button className='px-8 py-2 bg-[#4e0eff] rounded-xl cursor-pointer'>{buttonText}</button>
       </a>
     </BentoGrid>
