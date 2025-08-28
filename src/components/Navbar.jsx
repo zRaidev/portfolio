3d5af2b3360557335bed715514c6e8016647fdb0
macRaidev <isaac14e2007@gmail.com>
2025-08-28 09:33:56 -0500

    Navbar corrections

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,12 +4,12 @@ function Navbar () {
   return (
     <div className='w-full'>
       <header className='fixed top-0 z-50 flex h-16 w-full items-center px-4'>
-        <nav className='mx-auto h-10 w-25 md:w-2/5'>
+        <nav className='mx-auto h-10 w-24 md:w-2/5'>
           <ul
             className='
               flex flex-row items-center select-none overflow-x-auto rounded-2xl
               bg-[var(--surface)] text-[var(--text)] whitespace-nowrap
-              scrollbar-hidden justify-around max-md:pl-[14.5px] px-2 py-2
+              scrollbar-hidden justify-around max-md:pl-[14.6px] px-2 py-2
             '
           >
             {sections.map((x) => (
