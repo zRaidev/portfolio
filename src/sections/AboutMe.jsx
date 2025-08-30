dcdd32af9a517a80c19671a27aba52a514290a28
macRaidev <isaac14e2007@gmail.com>
2025-08-30 00:07:53 -0500

    AboutMe responsive finished, without picture in mobile

--- src/sections/AboutMe.jsx
+++ src/sections/AboutMe.jsx
@@ -3,13 +3,13 @@ import ProfilePicture from '../assets/images/linkedin-profile.webp'
 
 function AboutMe () {
   return (
-    <section id='about-me' className='h-[80vh]'>
-      <div className='flex flex-row items-center justify-center gap-10 p-4'>
-        <BentoGrid className='w-150 h-80 border-white/20'>
+    <section id='about-me' className='h-[80vh] flex justify-center'>
+      <div className='flex flex-col md:flex-row items-center justify-center gap-10 p-4'>
+        <BentoGrid className='w-auto md:w-150 h-auto border-white/20 m-10'>
           <h2 className='text-3xl font-semibold mb-5'>About me</h2>
           <p className='text-lg'>Hello! I'm Isaac, a Software Engineering student with an interest in web development and creating impactful technological solutions. I enjoy constantly learning, working on purpose-driven projects, and finding ways to improve user experience through design and functionality. I aspire to grow as a developer and add value by creating solutions to real-world problems.</p>
         </BentoGrid>
-        <BentoGrid className='w-80 h-80 !p-0 border-white/20!'>
+        <BentoGrid className='w-80 h-80 !p-0 border-white/20! hidden md:inline'>
           <img className='w-full h-full object-cover p-0.5 border-white/10 rounded-2xl' src={ProfilePicture} alt='Isaac Vergara picture' />
         </BentoGrid>
       </div>
