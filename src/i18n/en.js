6c9f0c38d187a7e481af7d03147a97e5380613dc
macRaidev <isaac14e2007@gmail.com>
2025-10-25 16:17:54 -0500

    Add English localization support and wrap App in LanguageProvider

---
+++ src/i18n/en.js
@@ -0,0 +1,13 @@
+export default {
+  sections: {
+    home: 'home',
+    aboutMe: ['about Me', 'about-me'],
+    habilities: 'habilities',
+    projects: 'projects',
+    contact: 'contact'
+  },
+  hiThere: 'Hi there!',
+  iAm: "I'm",
+  jobTitles: ['Web Developer', 'Frontend Developer', 'Software Engineer Student']
+
+}
--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './assets/styles/index.css'
 import App from './App.jsx'
 import ThemeProvider from './components/ThemeProvider.jsx'
+import { LanguageProvider } from './context/LanguageContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <ThemeProvider>
-      <App />
-    </ThemeProvider>
+    <LanguageProvider>
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
+    </LanguageProvider>
   </StrictMode>
 )
