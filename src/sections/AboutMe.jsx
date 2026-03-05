c8dc64d33bf2a59ed3c514a0d436817c3155c526
zRaidev <isaac14e2007@gmail.com>
2026-03-04 23:27:39 -0500

    Refactor AboutMe component to adjust BentoGrid class for improved layout consistency

--- src/sections/AboutMe.jsx
+++ src/sections/AboutMe.jsx
@@ -12,7 +12,7 @@ function AboutMe () {
           <h2 className='text-3xl font-semibold mb-5'>{t('sections.about-me')}</h2>
           <p className='text-lg'>{t('aboutMe.description')}</p>
         </BentoGrid>
-        <BentoGrid className='w-80 h-80 !p-0 border-white/20! hidden md:inline'>
+        <BentoGrid className='max-w-80 h-80 p-0! border-white/20! hidden md:inline'>
           <img className='w-full h-full object-cover p-0.5 border-white/10 rounded-2xl' src={ProfilePicture} alt='Isaac Vergara picture' />
         </BentoGrid>
       </div>
